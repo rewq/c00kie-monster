aca9f25f90a58d3b6457239af44d3e44293371d4
Dylan <dmurphymancini@gmail.com>
2016-05-03 11:17:42 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# c00kie-monster
-A Firefox and Chrome extension to view and edit cookies using the new cross-browser web extension API.
+# C00kie Monster
+A Firefox and Chrome extension for the viewing and editing of cookies. Coded with the new cross-browser web extension API.
+
+
+
+## TODO
+add about page in the options page
--- popup/c00kie-script.js
+++ popup/c00kie-script.js
@@ -4,39 +4,57 @@ If the user clicks on an element which has the class "ua-choice":
 * fetch the element's textContent: for example, "IE 11"
 * pass it into the background page's setUaString() function
 */
-console.log(chrome.cookies);
+//console.log(chrome.cookies);
+ $(function() {
+     $("#cookies").accordion({
+        collapsible: true, // Let's you squash all of the headings
+        //heightStyle: "content"
+    });
+  });
+
+
+// add delete all and clear
+var c00kies = "";
+
 
 $( document ).ready(function() { // Document Ready
 
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		if (tabs[0]) {
-		  currentTab = tabs[0];
-		  //console.log(currentTab);
+			currentTab = tabs[0];
 			chrome.cookies.getAll({"url":currentTab.url},function(cks){
-				for (var i = cks.length - 1; i >= 0; i--) {
+				c00kies = cks;
+				for (var i = 0; i <= cks.length-1; i++) {
 					var template = $("#cookie_template").html();
 					template = template.replace("{{ID}}",i);
+					template = template.replace("{{DOMAINNAME}}",cks[i].domain);
 					template = template.replace("{{NAME}}",cks[i].name);
 					template = template.replace("{{VALUE}}",cks[i].value);
 					template = template.replace("{{DOMAIN}}",cks[i].domain);
 					template = template.replace("{{PATH}}",cks[i].path);
 					template = template.replace("{{EXPIRE}}",cks[i].expirationDate);
-					template = template.replace("{{SECURE}}",(cks[i].secure ? "checked" : ""));
-					template = template.replace("{{SESSION}}",(cks[i].session ? "checked" : ""));
-					template = template.replace("{{HTTPONLY}}",(cks[i].httpOnly ? "checked" : ""));
-					template = template.replace("{{HOSTONLY}}",(cks[i].hostOnly ? "checked" : ""));
-					$("#cookies").append(template);
+					template = template.replace("{{SECURE}}",(cks[i].secure ? "checked" : " "));
+					template = template.replace("{{SESSION}}",(cks[i].session ? "checked" : " "));
+					template = template.replace("{{HTTPONLY}}",(cks[i].httpOnly ? "checked" : " "));
+					template = template.replace("{{HOSTONLY}}",(cks[i].hostOnly ? "checked" : " "));
+					$("#cookies").append(template).accordion("refresh");
 				}	
 			});
 		}
 	}); 
+
 }); // End Document Ready
 
 
 $(document).on('submit', '.cookie_form', function(e){ // Fired when a form is submitted
-    e.preventDefault(); // Disable the form from actually submitting
+	e.preventDefault(); // Disable the form from actually submitting
 
     var cookieID = this.id; // Cookie ID is the form ID
+
+    // save to background js
+    console.log(c00kies[cookieID]);
+    
+
     // Scraping inputs by name
     var cname = $("#"+cookieID+" h2[name=name]").text();
     var cdomain = $("#"+cookieID+" input[name=domain]").val();
@@ -58,7 +76,7 @@ $(document).on('submit', '.cookie_form', function(e){ // Fired when a form is su
 
 	//chrome.cookies.remove({"url": "http://localhost:8000/labs/session_example/session_example.php", "name": "session_example_cookie"}, function(deleted_cookie) { console.log(deleted_cookie); });
 
-	var expireDate = parseFloat(cexpire);
+	cexpire = parseFloat(cexpire); // If there was a decimal or comma-- the variable would be a string
 
 	var cookieData = {
 	    name: cname,
@@ -68,14 +86,25 @@ $(document).on('submit', '.cookie_form', function(e){ // Fired when a form is su
 	    secure: csecure,
 	    httpOnly: chttpOnly
 	};
-
 	if (!chostOnly) {
 		cookieData.domain = cdomain;
 	} 
 	if (!csession) {
-		cookieData.expirationDate = expireDate;
+		cookieData.expirationDate = cexpire;
 	} 
 
-	chrome.cookies.set(cookieData);
+	chrome.cookies.set(cookieData, onSet); // for callback chrome.cookies.set(cookieData, onSet);
 	
 }); // End Form Submit
+
+function onSet(cookie) {
+  if (chrome.runtime.LastError) {
+    console.error(chrome.runtime.LastError);
+  } else {
+    console.log(cookie);
+  }
+}
+
+ $(function() {
+    $( "#accordion" ).accordion();
+  });
